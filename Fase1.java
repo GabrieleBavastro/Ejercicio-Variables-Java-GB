d5c75effb821023a7480ca113e6e6658a5ea67dc
Gabriele Bavastro <gabrieleb84@hotmail.it>
2020-02-06 13:09:26 +0100

    Upload Fase 1.

---
+++ Fase1.java
@@ -0,0 +1,13 @@
+public class Fase1 {
+   public static void main(String[] args) {
+	String name = "Gabriele";
+	String surname1 = "Bavastro";
+	String surname2 = "Navone";
+	int day = 9;
+	int month = 7;
+	int year = 1984;
+		String fullName = surname1 + " " + surname2 + " " + name;
+		System.out.println("Full Name "+ fullName);
+		System.out.println("Birthday: "+ day + "/" + (month) + "/" + year);
+   }
+}
