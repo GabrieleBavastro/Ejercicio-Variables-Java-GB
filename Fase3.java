c049d04de58a264745c0c49a1caa7d31a15ad840
Gabriele Bavastro <gabrieleb84@hotmail.it>
2020-02-11 09:39:33 +0100

    Upload Fase3.

--- Fase3.java
+++ Fase3.java
@@ -1,11 +1,14 @@
 public class Fase3 {
 	  public static void main(String[] args) {
+	boolean b1 = true;
+	boolean b2 = false;	
 	int leapYearReference = 1948;
 	int yearOfBirth = 1984;
 	int leapYearloop = 4;
-    for (int i = leapYearReference; i <= yearOfBirth; i = i + leapYearloop) {
-    if (i == yearOfBirth) System.out.println("You born in " + i + " and it's a leap year");
-	else if (i == yearOfBirth) System.out.println("You born in " + i + " and it's not a leap year");
+    for (int i = leapYearReference; i <= yearOfBirth; i = i + leapYearloop)
+			{
+		if (i == yearOfBirth) {System.out.println(yearOfBirth + " it's your birthday and it's a leap year - " + b1);}
+		else {System.out.println(i + " or " + yearOfBirth + " it's your birthday and it's a leap year - " + b2);}
+		}
 	}  
   }
-}
