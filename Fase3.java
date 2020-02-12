58de556cb08c37c79a81a860e768b568727c643f
Gabriele Bavastro <gabrieleb84@hotmail.it>
2020-02-12 13:03:44 +0100

    Fase 3 y 4 Updated.

--- Fase3.java
+++ Fase3.java
@@ -2,13 +2,15 @@ public class Fase3 {
 	  public static void main(String[] args) {
 	boolean b1 = true;
 	boolean b2 = false;	
+	String stringBooleanTrue = " it's your birthday and it's a leap year - true";
+	String stringBooleanFalse = " it's your birthday and it's a leap year - false";
 	int leapYearReference = 1948;
 	int yearOfBirth = 1984;
 	int leapYearloop = 4;
     for (int i = leapYearReference; i <= yearOfBirth; i = i + leapYearloop)
 			{
-		if (i == yearOfBirth) {System.out.println(yearOfBirth + " it's your birthday and it's a leap year - " + b1);}
-		else {System.out.println(i + " or " + yearOfBirth + " it's your birthday and it's a leap year - " + b2);}
+		if (i == yearOfBirth) {System.out.println(yearOfBirth + stringBooleanTrue);}
+		else {System.out.println(i + stringBooleanFalse);}
 		}
 	}  
   }
--- Fase4.java
+++ Fase4.java
@@ -3,6 +3,8 @@ public class Fase4 {
 	String name = "Gabriele";
 	String surname1 = "Bavastro";
 	String surname2 = "Navone";
+	String stringBooleanTrue = " it's your birthday and it's a leap year - true";
+	String stringBooleanFalse = " it's your birthday and it's a leap year - false";
 	boolean b1 = true;
 	boolean b2 = false;	
 	int leapYearReference = 1948;
@@ -14,11 +16,11 @@ public class Fase4 {
 		System.out.println("Full Name: "+ fullName);
 		System.out.println("Birthday: "+ day + "/" + (month) + "/" + yearOfBirth);
 		
-	for (int i = leapYearReference; i <= yearOfBirth; i = i + leapYearloop)	
+	for (int i = leapYearReference; i <= yearOfBirth; i = i + leapYearloop)
+		
 	{		
-	if (i == yearOfBirth) {System.out.println("Your birthday it's a leap year - " + b1);}
-	else if (i < yearOfBirth) {System.out.println("Your birthday it's a leap year - " + b2);}
-	else {System.out.println("Your birthday it's a leap year - " + b2);}
-   }
+	if (i == yearOfBirth) {System.out.println(yearOfBirth + stringBooleanTrue);}
+	else {System.out.println(i + stringBooleanFalse);}
+	   }
 }
 }
