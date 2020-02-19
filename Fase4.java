d916b4d791af2cf225de7a5cf2ff3e4a9333c1ab
Gabriele Bavastro <gabrieleb84@hotmail.it>
2020-02-19 11:18:02 +0100

    Fase 4 Updated.

--- Fase4.java
+++ Fase4.java
@@ -2,23 +2,48 @@ public class Fase4 {
    public static void main(String[] args) {
 	String name = "Gabriele";
 	String surname1 = "Bavastro";
-	String surname2 = "Navone";
-	String stringBooleanTrue = " it's your birthday and it's a leap year - true";
-	String stringBooleanFalse = " it's your birthday and it's a leap year - false";	
-	int leapYearReference = 1948;
+	String surname2 = "Navone"; 
+	String stringBooleanTrue = "Your birthday and it's a leap year - true.";
+	String stringBooleanFalse = "Your birthday and it's a leap year - false.";	
 	int yearOfBirth = 1984;
-	int leapYearloop = 4;
 	int day = 9;
 	int month = 7;
 		String fullName = surname1 + " " + surname2 + " " + name;
 		System.out.println("Full Name: "+ fullName);
 		System.out.println("Birthday: "+ day + "/" + (month) + "/" + yearOfBirth);
 		
-	for (int i = leapYearReference; i <= yearOfBirth; i = i + leapYearloop)
-		
-	{		
-	if (i == yearOfBirth) {System.out.println(yearOfBirth + stringBooleanTrue);}
-	else {System.out.println(i + stringBooleanFalse);}
-	   }
+	switch (yearOfBirth) {
+  case (1948 + 4):
+    System.out.println(stringBooleanTrue);
+    break;
+  case (1948 + 4 + 4):
+	    System.out.println(stringBooleanTrue);
+	    break;
+  case (1948 + 4 + 4 + 4):
+	    System.out.println(stringBooleanTrue);
+	    break;
+  case (1948 + 4 + 4 + 4 + 4):
+	    System.out.println(stringBooleanTrue);
+	    break;
+  case (1948 + 4 + 4 + 4 + 4 + 4):
+	    System.out.println(stringBooleanTrue);
+	    break;
+  case (1948 + 4 + 4 + 4 + 4 + 4 + 4):
+	    System.out.println(stringBooleanTrue);
+	    break;
+  case (1948 + 4 + 4 + 4 + 4 + 4 + 4 + 4):
+	    System.out.println(stringBooleanTrue);
+	    break;
+  case (1948 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4):
+	    System.out.println(stringBooleanTrue);
+	    break;
+  case (1948 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4):
+	    System.out.println(stringBooleanTrue);
+	    break;
+  default:
+	    System.out.println(stringBooleanFalse);
+}
+	
+	   
 }
 }
